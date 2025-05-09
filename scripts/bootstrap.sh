b58a37bd5c2b10af03de446645bb6ccebe86bdab
Stefan Pejcic <stefan@pejcic.rs>
2025-05-09 19:45:26 +0200

    Update bootstrap.sh

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Permissions on directory /var/lib/tor/hidden_service/ are too permissive
 chmod 0700 /var/lib/tor/hidden_service
 
+# wait till we generate .onion address
 FILE=/var/lib/tor/hidden_service/hostname
 while [[ ! -f "$FILE" ]]
 do 
+  tor
   sleep 0.3
 done
 
-sh -l
+# keep it running
+tail -f /dev/null
